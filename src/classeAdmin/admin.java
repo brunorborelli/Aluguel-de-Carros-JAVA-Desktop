3be55be4aa156909203728450f44791490e8896d
Bruno Borelli <rodriguesborellibruno@gmail.com>
2022-09-21 20:07:47 -0300

    Primeiro commit -

---
+++ src/classeAdmin/admin.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classeAdmin;
+
+/**
+ *
+ * @author bruno
+ */
+public class admin {
+    
+    //Atributos
+    
+    private String usuarioAdm;
+    private String senhaAdm;
+    
+    
+    //Construtor
+
+    public admin(String usuarioAdm, String senhaAdm) {
+        this.usuarioAdm = usuarioAdm;
+        this.senhaAdm = senhaAdm;
+    }
+    
+    //Getters & Setters
+
+    public String getUsuarioAdm() {
+        return usuarioAdm;
+    }
+
+    public void setUsuarioAdm(String usuarioAdm) {
+        this.usuarioAdm = usuarioAdm;
+    }
+
+    public String getSenhaAdm() {
+        return senhaAdm;
+    }
+
+    public void setSenhaAdm(String senhaAdm) {
+        this.senhaAdm = senhaAdm;
+    }
+    
+    
+    
+    
+    
+}
